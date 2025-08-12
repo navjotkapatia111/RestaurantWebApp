c6047da06338e97aab334e8c7f04a7492231f14a
navjotkapatia111 <navjotkapatia111@gmail.com>
2025-08-12 17:40:48 +0530

    Success

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,4 @@
-import express from 'express'
-import 'dotenv/config'
-import { dbconnection } from './config/dbconnections.js'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
-import { router } from './routes/restaurant_routes.js'
-import cors from 'cors'
-import { route } from './routes/user_routes.js'
-import { items } from './routes/menu_routes.js'
-import { cart_route } from './routes/cart_routes.js'
-export const app = express()
-app.use(cors({
-  origin: '*'
-}));
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(cookieParser())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
-app.use('/api/v1/user',router)
-app.use('/api/v1/router',route)
-app.use('/api/v1/items',items)
-app.use('/api/v1/cart',cart_route)
+
 
 const port = process.env.PORT || 5000
 
