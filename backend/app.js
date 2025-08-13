799d9a8689f1db202ac5e1477b65368c00c62b49
navjotkapatia111 <navjotkapatia111@gmail.com>
2025-08-13 12:25:23 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,5 @@
-
-
-const port = process.env.PORT || 5000
-
-app.listen(port,async()=>{
-    await dbconnection()
-    console.log(`Server is running at ${port}`)
-})
+// const port = process.env.PORT || 5000
+//  app.listen(port,async()=>{
+//     await dbconnection()
+//     console.log(`Server is running at ${port}`)
+// })
